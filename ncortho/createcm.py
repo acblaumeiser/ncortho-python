7c1cbcd8412af130c6aaf2a5dcf4f6501c863c7c
Andreas Blaumeiser <ablaumeiser@stud.uni-frankfurt.de>
2018-12-13 17:45:03 +0100

    createcm works

--- ncortho/createcm.py
+++ ncortho/createcm.py
@@ -4,20 +4,44 @@
 import subprocess
 
 class CmConstructor(object):
-    def __init__(self, alignment, outpath):
+    def __init__(self, alignment, outpath, name, cpu):
         self.alignment = alignment
         self.outpath = outpath
+        self.name = name
+        self.cpu = cpu
+        self.model = '{0}/{1}.cm'.format(outpath, name)
     
     def construct(self):
-        construct_command = ''
-        ### cmbuild -n name of the cm
+        cmbuild = '/home/andreas/Applications/infernal-1.1.2-linux-intel-gcc/binaries/cmbuild'
+        construct_command = '{3} -n {0} -o {1}/{0}_cmbuild.log {1}/{0}.cm {2}'.format(self.name, self.outpath, self.alignment, cmbuild)
         subprocess.call(construct_command, shell=True)
-        return None
+        #return None
     
     def calibrate(self):
-        calibrate_command = ''
+        cmcalibrate = '/home/andreas/Applications/infernal-1.1.2-linux-intel-gcc/binaries/cmcalibrate'
+        calibrate_command = '{0} --cpu {1} {2}'.format(cmcalibrate, self.cpu, self.model)
         subprocess.call(calibrate_command, shell=True)
-        return None
+        #return None
+
+def main():
+    test_alignment = '/home/andreas/Documents/Internship/ncOrtho_to_distribute/ncortho_python/example/output/rna_aln.sto'
+    test_outpath = '/home/andreas/Documents/Internship/ncOrtho_to_distribute/ncortho_python/example/output'
+    test_name = 'mir-1'
+    test_cpu = 32
+    test_cc = CmConstructor(test_alignment, test_outpath, test_name, test_cpu)
+    test_cc.construct()
+    test_cc.calibrate()
+
+if __name__ == "__main__":
+    main()
+
+
+'''
+	system("$cmbuild -F $covariance_model $stockholm_aln");
+#	system("$cmbuild $covariance_model $stockholm_aln");
+	print "STEP 04\n";
+	system("$cmcalibrate --cpu $cpu $covariance_model");
+
 
 class CovarianceModel(object):
     def __init__(self):
@@ -25,3 +49,4 @@ class CovarianceModel(object):
         self.taxa = 'rat, hamster'
     def core_set(self):
         return None
+'''
