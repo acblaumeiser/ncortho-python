05b9fd45c0863e9d9c2bcf1f79d03ea93f5b7bd2
Andreas Blaumeiser <ablaumeiser@stud.uni-frankfurt.de>
2019-01-21 14:12:50 +0100

    coreset progress

--- ncortho/coreset.py
+++ ncortho/coreset.py
@@ -10,12 +10,16 @@
 
 
 import glob
+import cPickle as pickle
 
 core = glob.glob('/home/andreas/Documents/Internship/ncOrtho_to_distribute/ncortho_python/test_core_set_construction/core/*.gtf')
+ref_gtf = '/home/andreas/Documents/Internship/ncOrtho_to_distribute/ncortho_python/test_core_set_construction/ref_gtf'
 #core = glob.glob('/home/andreas/Documents/Internship/M.musculus_root/cm_retry/core/gtf/*.gtf')
-mirpath = '/home/andreas/Documents/Internship/ncOrtho_to_distribute/ncortho_python/test_core_set_construction/mirnas.txt'
+#mirpath = '/home/andreas/Documents/Internship/ncOrtho_to_distribute/ncortho_python/test_core_set_construction/mirnas.txt'
+mirpath = '/home/andreas/Documents/Internship/ncOrtho_to_distribute/ncortho_python/example/micrornas/mmu_mirna.tsv'
 #reference = '/home/andreas/Documents/Internship/ncOrtho_to_distribute/ncortho_python/test_core_set_construction/reference.gtf'
 reference = '/home/andreas/Documents/Internship/M.musculus_root/cm_retry/root/gtf/Mus_musculus.chromosomes.gtf'
+
 #query = '/home/andreas/Documents/Internship/ncOrtho_to_distribute/ncortho_python/test_core_set_construction/query.gtf'
 
 core.append(reference)
@@ -23,11 +27,11 @@ core.append(reference)
 gtf_dict = {}
 
 #mirnas = []
-
+c=0
 #for corespecies in core:
 def gtf_parser(corespecies):
     species = corespecies.split('/')[-1].split('.')[0]
-    print species
+    #print species
     gen_dict = {}
     chr_dict = {}
 
@@ -36,7 +40,7 @@ def gtf_parser(corespecies):
     #with open(inpath) as infile, open(outpath, 'wb') as outfile:
     with open(corespecies) as infile:
         for line in infile:
-            if not line.startswith('#') and line.split()[2] == 'gene':
+            if not line.startswith('#') and line.split()[2] == 'gene' and line.split('gene_biotype')[1].split('\"')[1] == 'protein_coding':
                 linedata = line.strip().split('\t')
                 contig = linedata[0]
                 geneid = linedata[-1].split('\"')[1]
@@ -60,43 +64,143 @@ def gtf_parser(corespecies):
     #    print chr_dict
     return chr_dict
 
+def ortho_search(r_gene):
+    for core_taxon in ortho_dict.keys():
+        try:
+            ortholog = ortho_dict[core_taxon][r_gene]
+                        #print '{0} is the ortholog for {1} in {2}.'.format(left_ortholog, ref_dict[chromo][len(ref_dict[chromo])][0], core_taxon)
+            print '{0} is the ortholog for {1} in {2}.'.format(ortholog, ref_dict[chromo][gene][0], core_taxon)
+        except:
+                        #print 'No ortholog for {0} found in {1}.'.format(ref_dict[chromo][len(ref_dict[chromo])][0], core_taxon)
+            print 'No ortholog for {0} found in {1}.'.format(ref_dict[chromo][gene][0], core_taxon)
+
+#omapaths = ['/home/andreas/Documents/Internship/M.musculus_root/cm_retry/oma/I.tridecemlineatus']
+omapaths = glob.glob('/home/andreas/Documents/Internship/ncOrtho_to_distribute/ncortho_python/test_core_set_construction/oma/*.*')
+
+ortho_dict = {}
+
+for omapath in omapaths:
+    taxon = omapath.split('/')[-1]
+    with open(omapath) as omafile:
+    #for line in omafile:
+        #print line.split()
+
+#        orthologs = {ref: core for (ref, core) in [(line.split()[0], line.split()[1]) for line in omafile.readlines() if len(line.split()) == 4]}
+        orthologs = {ref: core for (ref, core) in [(line.split()[0], line.split()[1]) for line in omafile.readlines()]}
+        ortho_dict[taxon] = orthologs
+
+#print len(orthologs)
+#print ortho_dict.keys()
+
+#d = {key: value for (key, value) in iterable}
+
+
 with open(mirpath) as mirfile:
     mirnas = [line.split() for line in mirfile.readlines() if not line.startswith('#')]
-    print mirnas
+    #print mirnas
 
 ### mir-1	chr1	51	100	+	GCTTGGGACACATACTTCTTTATATGCCCATATGAACCTGCTAAGCTATGGAATGTAAAGAAGTATGTATTTCAGGC	TGGAATGTAAAGAAGTATGTAT
 ### chr1	sgd	gene	1	50	.	+	.	gene_id "gene1";
 
-ref_dict = gtf_parser(reference)
-print ref_dict['14']
+#ref_dict = gtf_parser(reference)
+#with open(ref_gtf, 'wb') as outfile:
+#    pickle.dump(ref_dict, outfile, protocol=2)
+
+with open(ref_gtf, 'rb') as tmpfile:
+    ref_dict = pickle.load(tmpfile)
 
 for mirna in mirnas:
     mirid = mirna[0]
-    chromo = mirna[1]
+    print '### {0} ###'.format(mirid)
+    if 'chr' in mirna[1]:
+        chromo = mirna[1].split('chr')[1]
+    else:
+        chromo = mirna[1]
     start = int(mirna[2])
     end = int(mirna[3])
-### find left neighbor
-    if start < int(ref_dict[chromo][1][1]):
-        print 'No left neighbor for {0}.'.format(mirid)
-        found_left = False
+    strand = mirna[4]
+
+### find left neighbor or check if located inside gene
+### chr_dict[contig][i] = (geneid, start, end, strand)
+
+### case 1): there is no protein-coding gene on the same contig as the miRNA, so there can be no neighbors (should only occur in highly fragmented assemblies)
+    if not chromo in ref_dict.keys():
+        print 'There are no protein-coding genes on contig {0}. Synteny around {1} cannot be established.'.format(chromo, mirid)
+        continue
+
+### case 2): miRNA is located left of the first gene and hence has no left neighbor, the first gene is therefore by default the right neighbor
+    if end < int(ref_dict[chromo][1][1]):
+        print 'There is no left neighbor of {0}, since it is located at the start of contig {1}.'.format(mirid, chromo)
+        print '{0} is the right neighbor of {1}.'.format(ref_dict[chromo][1][0], mirid)
+        continue
+        #found_left = False
+
+### case 3): miRNA is located right to the last gene, so the last gene is the left neighbor and there cannot be a right neighbor
+
+    elif start > int(ref_dict[chromo][len(ref_dict[chromo])][2]):
+        #found_left = True
+        print '{0} is the left neighbor of {1}.'.format(ref_dict[chromo][len(ref_dict[chromo])][0], mirid)
+        print 'There is no right neighbor of {0}, since it is located at the end of contig {1}.'.format(mirid, chromo)
+        continue
+
+### case 4): miRNA is located either between two genes or inside (an intron of) a gene
+
     else:
-        for gene in ref_dict[chromo]:
-            if int(ref_dict[chromo][gene][2]) < start and ref_dict[chromo][gene+1][1] > end:
-                found_left = True
+        for i, gene in enumerate(ref_dict[chromo]):
+            gene_data = ref_dict[chromo][gene]
+            #print gene_data
+            ### case 4.1): miRNA inside gene
+            if start >= gene_data[1] and end <= gene_data[2] and strand == gene_data[3]:
+                c+=1
+                print '{0} is located inside the gene {1}.'.format(mirid, gene_data[0])
+                print mirna[:5]
+                print gene_data
+                ortho_search(gene_data[0])
+                #for core_taxon in ortho_dict.keys():
+                #    try:
+                        #print 'Do you even try?'
+                 #       left_ortholog = ortho_dict[core_taxon][ref_dict[chromo][len(ref_dict[chromo])][0]]
+                        #print '{0} is the ortholog for {1} in {2}.'.format(left_ortholog, ref_dict[chromo][len(ref_dict[chromo])][0], core_taxon)
+                  #      print '{0} is the ortholog for {1} in {2}.'.format(left_ortholog, ref_dict[chromo][gene][0], core_taxon)
+                   # except:
+                        #print 'No ortholog for {0} found in {1}.'.format(ref_dict[chromo][len(ref_dict[chromo])][0], core_taxon)
+                    #    print 'No ortholog for {0} found in {1}.'.format(ref_dict[chromo][gene][0], core_taxon)
+                continue
+            elif int(ref_dict[chromo][gene][2]) < start and ref_dict[chromo][gene+1][1] > end:
                 left = gene
-                print '{0}, {1} is the left neighbor of {2}.'.format(gene, ref_dict[chromo][gene][0], mirid)
-                #print ref_dict[chromo][1538]
-                #print ref_dict[chromo][gene+1]
+                print '{1} is the left neighbor of {2}.'.format(gene, ref_dict[chromo][gene][0], mirid)
+                ortho_search(gene_data[0])
+                #for core_taxon in ortho_dict.keys():
+                #    try:
+                        #print 'Do you even try?'
+                 #       left_ortholog = ortho_dict[core_taxon][ref_dict[chromo][len(ref_dict[chromo])][0]]
+                        #print '{0} is the ortholog for {1} in {2}.'.format(left_ortholog, ref_dict[chromo][len(ref_dict[chromo])][0], core_taxon)
+                  #      print '{0} is the ortholog for {1} in {2}.'.format(left_ortholog, ref_dict[chromo][gene][0], core_taxon)
+                   # except:
+                        #print 'No ortholog for {0} found in {1}.'.format(ref_dict[chromo][len(ref_dict[chromo])][0], core_taxon)
+                    #    print 'No ortholog for {0} found in {1}.'.format(ref_dict[chromo][gene][0], core_taxon)
+            #else:
+                #found_left = False
 
 ### find right neighbor (easy if left neighbor is known)
 #1798, ENSMUSG00000101445 is the left neighbor of mmu-mir-1a-1.
-    if found_left:
-        right = ref_dict[chromo][left+1][0]
-        print '{0}, {1} is the right neighbor of {2}.'.format(left+1, ref_dict[chromo][left+1][0], mirid)
-    else:
-        print 'No left neighbor for {0}, how about right?'.format(mirid)
+    #if found_left:
+     #   try:
+      #      right = ref_dict[chromo][left+1][0]
+       #     print '{1} is the right neighbor of {2}.'.format(left+1, ref_dict[chromo][left+1][0], mirid)
+        #except:
+         #   print 'No right neighbor for {0}.'.format(mirid)
+ #   else:
+#        print ref_dict[chromo]
+ #       print chromo
+    #    if end < ref_dict[chromo][1][1]:
+     #       print '{0} is the right neighbor of {1}.'.format(ref_dict[chromo][1][0], mirid)
+            
+        #print 'No left neighbor for {0}, how about right?'.format(mirid)
 
 #    print ref_dict[chromo]
+#print ortho_dict
+print c
     
 
 class CoreSet:
