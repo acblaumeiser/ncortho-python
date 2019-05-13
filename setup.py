e826a104e0a0d0ebd5515a52a0e96e132c62d6d6
Andreas Blaumeiser <ablaumeiser@stud.uni-frankfurt.de>
2019-05-13 16:49:58 +0200

    included setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
-from setuptools import find_packages, setup
+import setuptools
+#from setuptools import find_packages, setup
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ncortho',
+    version='1.0',
+    author='Andreas Blaumeiser',
+    author_email='',
+    description='ncRNA orthology search',
+    long_description= long_description,
+    url='',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': ['ncortho = ncortho.ncortho:main'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: ',
+        'Operating System :: Linux',
+    ],
+)
+
+
 
 #setup, check if the required third-party tools are available
 
